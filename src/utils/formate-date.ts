ea19303031eb4bb4ac1a8757865398d46af93034
Pool Llerena <pool_1541@hotmail.com>
2025-06-11 12:08:05 -0500

    chore: add formateDate utility function for localized date formatting

---
+++ src/utils/formate-date.ts
@@ -0,0 +1,12 @@
+export function formateDate(date: Date): string {
+  return date.toLocaleString("es-PE", {
+    timeZone: "America/Lima",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+}
