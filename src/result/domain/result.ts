ae034b5460a49a65f77f349bdc94183a2bf052d1
pool_1541 <pool_1541@hotmail.com>
2025-08-02 15:12:56 -0500

    feat(result): add result entity

---
+++ src/result/domain/result.ts
@@ -0,0 +1,115 @@
+import { UserId } from "./value-objects/user-id";
+import { ResultId } from "./value-objects/result-id";
+import { ResultError } from "./errors/result-error";
+import { ResultOpened } from "./value-objects/result-opened";
+import { ResultCreatedAt } from "./value-objects/created-at";
+
+export type EmailClient = "hotmail" | "outlook" | "gmail" | "apple_mail";
+
+export class Result {
+  id: ResultId | null;
+  createdAt: ResultCreatedAt;
+  openend: ResultOpened;
+  emailClient: EmailClient;
+  userId: UserId;
+  resultUrl: string | null;
+  messageId: string;
+
+  constructor({
+    id,
+    createdAt,
+    openend,
+    emailClient,
+    userId,
+    resultUrl,
+    messageId,
+  }: {
+    id?: ResultId;
+    createdAt: ResultCreatedAt;
+    openend: ResultOpened;
+    emailClient: EmailClient;
+    userId: UserId;
+    resultUrl?: string;
+    messageId: string;
+  }) {
+    this.id = id ?? null;
+    this.createdAt = createdAt;
+    this.openend = openend;
+    this.emailClient = emailClient;
+    this.userId = userId;
+    this.resultUrl = resultUrl ?? null;
+    this.messageId = messageId;
+  }
+
+  getId(): ResultId | null {
+    return this.id;
+  }
+
+  isPersisted(): boolean {
+    return this.id !== null;
+  }
+
+  requireId(): ResultId {
+    if (!this.id) throw new ResultError("Result must be persisted to access ID");
+
+    return this.id;
+  }
+
+  getCreatedAt(): ResultCreatedAt {
+    return this.createdAt;
+  }
+
+  getOpened(): ResultOpened {
+    return this.openend;
+  }
+
+  getEmailClient(): EmailClient {
+    return this.emailClient;
+  }
+
+  getUserId(): UserId {
+    return this.userId;
+  }
+
+  getResultUrl(): string | null {
+    return this.resultUrl;
+  }
+
+  requireResultUrl(): string {
+    if (!this.resultUrl) throw new ResultError("Result must be persisted to access URL");
+
+    return this.resultUrl;
+  }
+
+  getMessageId(): string {
+    return this.messageId;
+  }
+
+  equals(other: Result): boolean {
+    if (!this.isPersisted() || !other.isPersisted()) return false;
+
+    return this.requireId().equals(other.requireId());
+  }
+
+  toJson(): object {
+    return {
+      id: this.requireId().getValue(),
+      createdAt: this.createdAt.getValue(),
+      openend: this.openend.getValue(),
+      emailClient: this.emailClient,
+      userId: this.userId.getValue(),
+      resultUrl: this.resultUrl,
+      messageId: this.messageId,
+    };
+  }
+
+  static create({ emailClient, messageId, userId }: { emailClient: EmailClient; userId: UserId; messageId: string }): Result {
+    return new Result({
+      createdAt: new ResultCreatedAt(new Date()),
+      openend: new ResultOpened(false),
+      emailClient,
+      userId,
+      messageId,
+    });
+  }
+}
