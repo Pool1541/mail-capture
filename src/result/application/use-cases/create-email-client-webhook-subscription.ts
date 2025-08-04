0faa4fb2843fe1b399c7fe80c983de6052a652bd
pool_1541 <pool_1541@hotmail.com>
2025-08-04 09:44:26 -0500

    feat(result): add create-email-client-webwook-subscription use case

---
+++ src/result/application/use-cases/create-email-client-webhook-subscription.ts
@@ -0,0 +1,50 @@
+import { type IEmailClientService } from "@/result/domain/contracts/email-client-service";
+
+export class CreateEmailClientWebhookSubscription {
+  private readonly endpoint = "https://graph.microsoft.com/v1.0/subscriptions";
+  constructor(private readonly emailClientService: IEmailClientService) {}
+
+  async execute() {
+    const fiveDaysFromNow = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000);
+
+    const payload = {
+      changeType: "created",
+      clientState: process.env.OUTLOOK_CLIENT_STATE ?? "",
+      notificationUrl: "https://bgw1r9rz-8080.brs.devtunnels.ms/result/webhook",
+      resource: `users/${process.env.EMAIL ?? ""}/messages`,
+      expirationDateTime: fiveDaysFromNow.toISOString(),
+    };
+
+    const accesToken = await this.emailClientService.getAccessToken();
+
+    const requetInit: RequestInit = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accesToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      const response = await fetch(this.endpoint, requetInit);
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(errorData);
+        throw new Error(`Error creating webhook subscription: ${response.status.toString()} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error creating webhook subscription:", error.message);
+        throw error;
+      } else {
+        console.error("An unexpected error occurred:", error);
+        throw new Error("An unexpected error occurred while creating webhook subscription.");
+      }
+    }
+  }
+}
