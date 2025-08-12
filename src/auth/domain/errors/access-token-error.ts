d2e10b59457dbd7f6e57ecd56cde2a405adc2b99
pool_1541 <pool_1541@hotmail.com>
2025-08-11 22:46:48 -0500

    feat(auth): remove AccessTokenError and AccessTokenExpiredError classes

--- src/auth/domain/errors/access-token-error.ts
+++
@@ -1,7 +0,0 @@
-export class AccessTokenError extends Error {
-  constructor(message?: string) {
-    super(message ?? "Invalid access token");
-    this.name = "AccessTokenError";
-    Object.setPrototypeOf(this, AccessTokenError.prototype);
-  }
-}
--- src/auth/domain/errors/access-token-expired.ts
+++
@@ -1,7 +0,0 @@
-export class AccessTokenExpiredError extends Error {
-  constructor(message?: string) {
-    super(message ?? "Access token has expired");
-    this.name = "AccessTokenExpiredError";
-    Object.setPrototypeOf(this, AccessTokenExpiredError.prototype);
-  }
-}
