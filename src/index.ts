75ff6f7d9469d951fa43866871a6c9712b7cfe94
pool_1541 <pool_1541@hotmail.com>
2025-12-12 18:53:10 -0500

    refactor: update error handling and reorganize middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Application, urlencoded, type NextFunction, type Request, type Response } from "express";
 
 import { userRouter } from "@/user/infrastructure/express-user-router";
@@ -9,6 +8,7 @@ import { ScrapperWorker } from "@/result/infrastructure/workers/scrapper-worker"
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import { NotFoundError } from "./shared/domain/errors";
 
 class Server {
   private app: Application;
@@ -19,25 +19,15 @@ class Server {
     this.port = process.env.PORT ?? "8080";
     this.initializeMiddlewares();
     this.initializeRoutes();
+    this.initializeErrorHandling();
   }
 
   private initializeMiddlewares(): void {
+    this.app.use(helmet());
     this.app.use(cors());
     this.app.use(morgan("dev"));
-    this.app.use(helmet());
     this.app.use(express.json());
     this.app.use(urlencoded({ extended: true }));
-    this.app.use((req: Request, res: Response) => {
-      res.status(404).json({
-        error: {
-          code: "ROUTE_NOT_FOUND",
-          message: "Route not found",
-          path: req.path,
-          method: req.method,
-        },
-      });
-    });
-    this.app.use(globalErrorHandler);
   }
 
   private initializeRoutes(): void {
@@ -45,6 +35,17 @@ class Server {
     this.app.use("/auth", authRouter);
     this.app.use("/result", resultRouter);
   }
+
+  private initializeErrorHandling(): void {
+    this.app.use(this.handle404);
+    this.app.use(globalErrorHandler);
+  }
+
+  private handle404 = (req: Request, res: Response, next: NextFunction): void => {
+    const error = new NotFoundError("HTTP", `${req.method} ${req.path}`);
+
+    next(error);
+  };
 
   public listen(): void {
     this.app.listen(this.port, () => {
