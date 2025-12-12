e10f25dc25e1feb7a2eb8147e1f8a140b2f965e8
pool_1541 <pool_1541@hotmail.com>
2025-12-12 14:06:16 -0500

    refactor(result): remove simulated delay and streamline scrapper execution

--- src/result/infrastructure/services/scrapper-service.ts
+++ src/result/infrastructure/services/scrapper-service.ts
@@ -1,10 +1,15 @@
+import Scrape from "../../../../scrap-outlook-v2";
+
 export async function runScrapper(body: unknown) {
   console.log("Running scrapper...", { body });
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("Scrapper finished successfully.");
-      resolve(true);
-    }, 30000); // Simulate a 30 seconds scrapping process
-  });
+  const { subject, sender } = body as { subject: string; sender: string };
+
+  await Scrape({ subject, sender });
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     console.log("Scrapper finished successfully.");
+  //     resolve(true);
+  //   }, 30000); // Simulate a 30 seconds scrapping process
+  // });
 }
