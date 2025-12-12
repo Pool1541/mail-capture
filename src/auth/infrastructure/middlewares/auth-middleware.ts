704f83bc4d189b6772f2e95f6f618075766af0ab
pool_1541 <pool_1541@hotmail.com>
2025-12-12 17:41:00 -0500

    refactor(auth): simplify error handling in auth middleware

--- src/auth/infrastructure/middlewares/auth-middleware.ts
+++ src/auth/infrastructure/middlewares/auth-middleware.ts
@@ -2,30 +2,20 @@ import type { NextFunction, Request, Response } from "express";
 
 import { AccessToken } from "@/auth/domain/access-token";
 import { ServiceContainer } from "@/shared/infrastructure/service-container";
+import { MissingAccessTokenError } from "@/auth/domain/errors";
 
 export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const authorizationHeader = req.headers.authorization;
 
   if (!authorizationHeader?.startsWith("Bearer ")) {
-    res.status(401).json({ error: "Missing JWT" });
-    return;
+    throw new MissingAccessTokenError();
   }
 
   const token = authorizationHeader.split(" ")[1];
 
-  try {
-    const accessToken: AccessToken = await ServiceContainer.auth.validateAccessToken.execute(token);
-    req.user = accessToken.getUserMetadata();
-    req.token = token;
-    console.log({ req: req.user });
-    next();
-  } catch (error) {
-    console.log(error);
-    if (error instanceof Error) {
-      res.status(401).json({ error: "Access token is invalid or expired" });
-      return;
-    }
-    res.status(500).json({ error: "Internal server error" });
-    return;
-  }
+  const accessToken: AccessToken = await ServiceContainer.auth.validateAccessToken.execute(token);
+  req.user = accessToken.getUserMetadata();
+  req.token = token;
+
+  next();
 }
