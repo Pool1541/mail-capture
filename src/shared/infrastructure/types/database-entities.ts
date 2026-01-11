922c4e5c664dc93cde095d0bd0e32f659027613d
pool_1541 <pool_1541@hotmail.com>
2026-01-11 00:36:18 -0500

    chore(shared): add SupabaseWebhookEntity type

--- src/shared/infrastructure/types/database-entities.ts
+++ src/shared/infrastructure/types/database-entities.ts
@@ -2,4 +2,5 @@ import type { Constants, Tables } from "./supabase";
 
 export type SupabaseResultEntity = Tables<"results">;
 export type SupabaseUserEntity = Tables<"users">;
+export type SupabaseWebhookEntity = Tables<"webhooks">;
 export type SupabaseEmailClients = (typeof Constants.public.Enums.email_clients)[number];
